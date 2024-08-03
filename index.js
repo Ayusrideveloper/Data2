2e284b72ef6ef6c679fa8e2359df03c762c962e8
Ayusrideveloper <swastikpattnaik924@gmail.com>
2024-08-03 09:03:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+[
+    {
+      "location_id": 1,
+      "location_name": "Buguda,Ganjam",
+      "state_id": 22,
+      "state": "Odisha",
+      "country_name": "India"
+    },
+    {
+      "location_id": 2,
+      "location_name": "sarojini market, Delhi",
+      "state_id": 8,
+      "state": "Delhi",
+      "country_name": "India"
+    },
+    {
+      "location_id": 3,
+      "location_name": "bhutbangal market,Lucknow",
+      "state_id": 28,
+      "state": "Lucknow",
+      "country_name": "India"
+    },
+    {
+      "location_id": 4,
+      "location_name": "Textile market,surat",
+      "state_id": 10,
+      "state": "Gujurat",
+      "country_name": "India"
+    },
+    {
+      "location_id": 5,
+      "location_name": "Hadapsar, Pune",
+      "state_id": 17,
+      "state": "Maharashtra",
+      "country_name": "India"
+    },
+    {
+      "location_id": 6,
+      "location_name": "Kothrud, Pune",
+      "state_id": 17,
+      "state": "Maharashtra",
+      "country_name": "India"
+    }
+    
+  ]
